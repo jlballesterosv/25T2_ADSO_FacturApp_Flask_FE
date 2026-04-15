df2433003a1f4aa1ba56d7db06438a32cd3d540c
Jorge Ballesteros <joba1021@netcracker.com>
2026-04-14 21:30:25 -0500

    2026-04-14-clase-002-rutas

--- src/app.py
+++ src/app.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# flas application start
-
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/lista-clientes")
+def listaClientes():
+    return render_template("ListaClientes.html")
+
+@app.route("/nuevo-cliente")
+def nuevoCliente():
+    return render_template("NuevoCliente.html")
